280f2626357cfcb559f42b07986e42909c60af26
Kurt <kurt@murrell.xyz>
2019-08-18 21:19:07 +1000

    Refactor/formatter (#5)

--- src/Serilogs.Sinks.Firestore/FirestoreSink.cs
+++ src/Serilogs.Sinks.Firestore/FirestoreSink.cs
@@ -1,12 +1,12 @@
 ﻿using Serilog.Events;
+using Serilog.Formatting.Compact;
+using Serilog.Sinks.Firestore.Interfaces;
+using Serilog.Sinks.Firestore.Models;
 using Serilog.Sinks.PeriodicBatching;
 using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using System.Linq;
-using Serilog.Formatting.Compact;
-using Serilog.Sinks.Firestore.Models;
-using Serilog.Formatting.Json;
+using System.Threading.Tasks;
 
 namespace Serilog.Sinks.Firestore
 {
@@ -16,22 +16,30 @@ namespace Serilog.Sinks.Firestore
     internal class FirestoreSink : PeriodicBatchingSink
     {
         private readonly IFormatProvider _formatProvider;
-        private readonly FirestoreApiClient _firestoreClient;
-        private readonly JsonFormatter _formatter;
+        private readonly IFirestoreApiClient _firestoreClient;
 
         /// <summary>
         /// Initialises a new instance of <see cref="FirestoreSink"/> 
         /// </summary>
         /// <param name="formatProvider">Message formatter</param>
-        /// <param name="configuration"></param>
+        /// <param name="configuration">Firestore specific configuration</param>
         /// <param name="batchInterval">The time to wait between checking for event batches</param>
         /// <param name="batchSizeLimit">The maximum number of events to include in a single batch, Firestore limit is 500</param>
         internal FirestoreSink(IFormatProvider formatProvider, FirestoreConfiguration configuration, TimeSpan batchInterval, int batchSizeLimit)
+            : this(formatProvider, new FirestoreApiClient(configuration), batchInterval, batchSizeLimit) { }
+
+        /// <summary>
+        /// Initialises a new instance of <see cref="FirestoreSink"/> 
+        /// </summary>
+        /// <param name="formatProvider">Message formatter</param>
+        /// <param name="firestoreClient">Firestore client</param>
+        /// <param name="batchInterval">The time to wait between checking for event batches</param>
+        /// <param name="batchSizeLimit">The maximum number of events to include in a single batch, Firestore limit is 500</param>
+        internal FirestoreSink(IFormatProvider formatProvider, IFirestoreApiClient firestoreClient, TimeSpan batchInterval, int batchSizeLimit)
             : base(batchSizeLimit, batchInterval)
         {
             _formatProvider = formatProvider;
-            _firestoreClient = new FirestoreApiClient(configuration);
-            _formatter = new JsonFormatter();
+            _firestoreClient = firestoreClient;
         }
 
         /// <summary>
@@ -70,7 +78,7 @@ namespace Serilog.Sinks.Firestore
         /// <returns>A dictionary containing serialised properties</returns>
         internal Dictionary<string, string> SerialiseData(IReadOnlyDictionary<string, LogEventPropertyValue> properties)
         {
-           return properties.ToDictionary(x => x.Key, v => v.Value.ToString());
+            return properties.ToDictionary(x => x.Key, v => v.Value?.ToString(null, _formatProvider));
         }
     }
 }
